12077e8237129c1d73f60e3406e7d3acb9ec0a2f
ixp123 <77308732+ixp123@users.noreply.github.com>
2021-02-05 23:42:57 +0000

    Add files via upload

--- auto-setup.sh
+++ auto-setup.sh
@@ -12,7 +12,7 @@ sudo pacman -S xorg-server
 sudo pacman -S xinit
 sudo pacman -S gimp
 sudo pacman -S discord
-sudo pacman -S neovim
+sudo pacman -S emacs
 sudo pacman -S consolas-font
 
 #Cloning repos
@@ -20,7 +20,10 @@ git clone https://github.com/ixp123/dwmconf
 git clone https://github.com/ixp123/stconf
 git clone https://github.com/ixp123/slstatusconf
 git clone https://github.com/ixp123/myfonts
+git clone https://github.com/ixp123/bashrc
 git clone https://git.suckless.org/dmenu
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+
 
 #Compile applications
 cd dwmconf
@@ -29,7 +32,7 @@ cp xinitrc ~/.xinitrc
 cd dwm
 sudo make clean install
 
-cd ~/github/st/st-0.8.4
+cd ~/github/stconf/st-0.8.4
 sudo make clean install
 
 cd ~/github/slstatusconf/slstatus
@@ -40,8 +43,13 @@ cp 'Font Awesome 5 Brands-Regular-400.otf' ~/.fonts
 cp 'Font Awesome 5 Free-Regular-400.otf' ~/.fonts
 cp 'Font Awesome 5 Free-Solid-900.otf' ~/.fonts
 
+cd ~/github/bashrc/
+cp bashrc ~/.bashrc
+
 cd ~/github/dmenu
 sudo make clean install
+
+~/.emacs.d/bin/doom install
 
 #Updates the system 
 sudo pacman -Syyu
